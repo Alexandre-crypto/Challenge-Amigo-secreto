eae16b14f64eb741308ddd62f1ef020383b92a65
Alexandre-crypto <alexandre21dantas@gmail.com>
2025-02-16 15:34:43 -0300

    Função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+    console.log("Amigos:", amigos); // Exibir no Console
+    input.value = "";
+    atualizarLista();
+}

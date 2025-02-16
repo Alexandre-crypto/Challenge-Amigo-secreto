b175dfddf82918ea9945d1e6af60fca1241f8c0b
Alexandre-crypto <alexandre21dantas@gmail.com>
2025-02-16 15:50:15 -0300

    Função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ function adicionarAmigo() {
     input.value = "";
     atualizarLista();
 }
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let amigo of amigos) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }
+}
